37bfd4451a7a3bb2e5b38e18c304136ccb0cded5
Sunilkumarjust4u <106077080+Sunilkumarjust4u@users.noreply.github.com>
2024-11-28 00:53:01 +0530

    Update SAY.py

--- SAY.py
+++ SAY.py
@@ -1,1 +1,21 @@
 # Python Learn with fun
+import cowsay
+import sys 
+
+if len (sys.argv)==2:
+    cowsay.trex("hello,"+sys.argv[1])
+    cowsay.dragon("hello,"+sys.argv[1])
+    cowsay.tux("hello,"+sys.argv[1])
+    cowsay.stegosaurus("hello,"+sys.argv[1])
+    cowsay.turtle("hello,"+sys.argv[1])
+    
+    cowsay.ghostbusters("hello,"+sys.argv[1])
+    cowsay.kitty("hello,"+sys.argv[1])
+    cowsay.turkey("hello,"+sys.argv[1])
+    cowsay.cheese("hello,"+sys.argv[1])
+    cowsay.beavis("hello,"+sys.argv[1])
+    cowsay.daemon("hello,"+sys.argv[1])
+    cowsay.fox("hello,"+sys.argv[1])
+    cowsay.meow("hello,"+sys.argv[1])
+    cowsay.octopus("hello,"+sys.argv[1])
+    
